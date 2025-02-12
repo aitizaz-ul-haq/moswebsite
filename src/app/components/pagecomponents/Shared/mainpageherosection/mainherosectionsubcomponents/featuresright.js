8b57785545d9e7f5912bf931458ec5ab413d848a
aitizaz <atz.softprgmr@gmail.com>
2025-02-12 18:06:47 +0500

    added changes

--- src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/featuresright.js
+++ src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/featuresright.js
@@ -1,15 +1,5 @@
 import "./styles/featuresright.css";
 
 export default function FeaturesRight({ title, paragraph }) {
-  return (
-    <div className="hero-content-features">
-      <h1 className="hero-title-features font-poppins">{title}</h1>
-      <div className="hero-paragraph-container-features">
-        <p className="hero-paragraph-features font-nunito">{paragraph}</p>
-      </div>
-      <div className="hero-button-container-features">
-        <div className="hero-button-features font-nunito">TALK TO US NOW</div>
-      </div>
-    </div>
-  );
+  return <>featres page</>;
 }
