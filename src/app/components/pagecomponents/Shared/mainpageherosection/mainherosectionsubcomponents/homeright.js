81b4c19185ef3f62d0ca22c9d10a2be5924e5a70
aitizaz <atz.softprgmr@gmail.com>
2025-02-12 20:17:50 +0500

    added changes

--- src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/homeright.js
+++ src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/homeright.js
@@ -2,8 +2,20 @@ import "./styles/homeright.css";
 
 export default function HomeRight({ title, paragraph }) {
   return (
-    <>
-      Homepage title {title} {paragraph}
-    </>
+    <div className="homeright-text-wrapper">
+      <div className="homeright-text-container">
+        <div className="homeright-title-container">
+          <h1 className="homeright-title font-poppins">{title}</h1>
+        </div>
+        <div className="homeright-paragraph-container">
+          <p className="homeright-paragraph font-nunito">{paragraph}</p>
+        </div>
+        <div className="homeright-button-container">
+          <button className="homeright-button font-nunito">
+            TALK TO US NOW
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
