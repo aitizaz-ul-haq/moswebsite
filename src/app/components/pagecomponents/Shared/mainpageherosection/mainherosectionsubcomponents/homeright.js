6f0d8a51d56bfb8e0888acedf5645c75a444afb1
aitizaz <atz.softprgmr@gmail.com>
2025-02-12 18:25:52 +0500

    added changes

--- src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/homeright.js
+++ src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/homeright.js
@@ -1,3 +1,9 @@
+import "./styles/homeright.css";
+
 export default function HomeRight({ title, paragraph }) {
-  return <>Homepage</>;
+  return (
+    <>
+      Homepage title {title} {paragraph}
+    </>
+  );
 }
