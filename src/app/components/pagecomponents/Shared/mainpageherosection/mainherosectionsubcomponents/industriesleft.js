a0cb951e2085cf1d8ef23c98ff4eabdd9a7ce89d
aitizaz <atz.softprgmr@gmail.com>
2025-02-13 13:34:21 +0500

    added changes

--- src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/industriesleft.js
+++ src/app/components/pagecomponents/Shared/mainpageherosection/mainherosectionsubcomponents/industriesleft.js
@@ -1,5 +1,21 @@
 import "./styles/industriesleft.css";
 
-export default function IndustriesLeft({ title, paragraph }) {
-  return <>INdustries page</>;
+export default function HomeRight({ title, paragraph }) {
+  return (
+    <div className="industriesleft-text-wrapper">
+      <div className="industriesleft-text-container">
+        <div className="industriesleft-title-container">
+          <h1 className="industriesleft-title font-poppins">{title}</h1>
+        </div>
+        <div className="industriesleft-paragraph-container">
+          <p className="industriesleft-paragraph font-nunito">{paragraph}</p>
+        </div>
+        <div className="industriesleft-button-container">
+          <button className="industriesleft-button font-nunito">
+            TALK TO US NOW
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
