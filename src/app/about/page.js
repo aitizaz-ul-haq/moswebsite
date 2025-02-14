4cc0757e9cbae6fa7f5d2933001c55cd4a5b4854
aitizaz <atz.softprgmr@gmail.com>
2025-02-14 17:45:08 +0500

    added changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,23 @@
 import MainPageHeroSection from "../components/pagecomponents/Shared/mainpageherosection/mainpageherosection";
 import AboutHeroData from "@/app/data/shareddata/hersectiondata.json";
+import Aboutheadingsectiondata from "@/app/data/shareddata/mainheadingsectiondata.json";
+import MainHeadingSection from "../components/pagecomponents/Shared/mainheadingsection/mainheadingsection";
 
 export default function About() {
   return (
-    <MainPageHeroSection
-      title={AboutHeroData.about.title}
-      paragraph={AboutHeroData.about.paragraph}
-      textOrientation={AboutHeroData.about.textOrientation}
-      backgroundImages={AboutHeroData.about.backgroundImages}
-    />
+    <>
+      <MainPageHeroSection
+        title={AboutHeroData.about.title}
+        paragraph={AboutHeroData.about.paragraph}
+        textOrientation={AboutHeroData.about.textOrientation}
+        backgroundImages={AboutHeroData.about.backgroundImages}
+      />
+      <MainHeadingSection
+        title={Aboutheadingsectiondata.about.title}
+        description={Aboutheadingsectiondata.about.description}
+        tagtext={Aboutheadingsectiondata.about.tagtext}
+        backgroundImages={Aboutheadingsectiondata.about.backgroundImages}
+      />
+    </>
   );
 }
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,24 @@
 import MainPageHeroSection from "../components/pagecomponents/Shared/mainpageherosection/mainpageherosection";
+import MainHeadingSection from "../components/pagecomponents/Shared/mainheadingsection/mainheadingsection";
 import ContactHeroData from "@/app/data/shareddata/hersectiondata.json";
+import Connectheadingsectiondata from "@/app/data/shareddata/mainheadingsectiondata.json";
 
 export default function Connect() {
   return (
-    <MainPageHeroSection
-      title={ContactHeroData.connect.title}
-      paragraph={ContactHeroData.connect.paragraph}
-      textOrientation={ContactHeroData.connect.textOrientation}
-      backgroundImages={ContactHeroData.connect.backgroundImages}
-    />
+    <>
+      <MainPageHeroSection
+        title={ContactHeroData.connect.title}
+        paragraph={ContactHeroData.connect.paragraph}
+        textOrientation={ContactHeroData.connect.textOrientation}
+        backgroundImages={ContactHeroData.connect.backgroundImages}
+      />
+
+      <MainHeadingSection
+        title={Connectheadingsectiondata.connect.title}
+        description={Connectheadingsectiondata.connect.description}
+        tagtext={Connectheadingsectiondata.connect.tagtext}
+        backgroundImages={Connectheadingsectiondata.connect.backgroundImages}
+      />
+    </>
   );
 }
