c10f87ee6e923b9e5ac6b57b32d4a9953bc3acd2
aitizaz <atz.softprgmr@gmail.com>
2025-03-05 10:52:01 +0500

    added changes

--- src/app/components/pagecomponents/homepagecomponents/homeindsubcomponents/homeindheading.js
+++ src/app/components/pagecomponents/homepagecomponents/homeindsubcomponents/homeindheading.js
@@ -1,7 +1,12 @@
 export default function HomeIndHeading({ Homeindustriessectiondata }) {
   return (
-    <div className="homepage-industries-tag font-poppins">
-      {Homeindustriessectiondata.homeindustriesheading}
-    </div>
+    <>
+      <div className="homepage-industres-mobile-heading font-poppins">
+        Industries
+      </div>
+      <div className="homepage-industries-tag font-poppins">
+        {Homeindustriessectiondata.homeindustriesheading}
+      </div>
+    </>
   );
 }
