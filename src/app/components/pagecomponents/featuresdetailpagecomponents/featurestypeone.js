b3eafd46f017dc99fb3fb4e2478b3b758d1294e0
aitizaz <atz.softprgmr@gmail.com>
2025-03-18 10:15:46 +0500

    added changes

--- src/app/components/pagecomponents/featuresdetailpagecomponents/featurestypeone.js
+++ src/app/components/pagecomponents/featuresdetailpagecomponents/featurestypeone.js
@@ -1,36 +1,41 @@
 import MainPageHeroSection from "../Shared/mainpageherosection/mainpageherosection";
-import Webdevdata from "../../../data/shareddata/hersectiondata.json";
 import TypeOneCardSection from "./typeonesubcomps/typeonecardsection";
-import Webdevcarddata from "../../../data/shareddata/cardcollectiondata.json";
 import FeaturesMethodTypeOne from "./featuresmethodtypeone";
-import Exampledata from "../../../data/homedata/homecardcollectiondata.json";
 import MainHeadingSection from "../Shared/mainheadingsection/mainheadingsection";
-import Mainheadingsectiondata from "../../../data/shareddata/mainheadingsectiondata.json";
 import CallToAction from "../Shared/calltoaction/calltoaction";
-import Calltoactiondata from "../../../data/shareddata/calltoactiondata.json";
+
 import "./styles/featurestypeone.css";
 
-export default function FeaturesTypeOne() {
-  let cards = Webdevcarddata.webdevelopment.cards;
-  let cardssecond = Exampledata.features.cards;
+export default function FeaturesTypeOne({
+  Webdevherotitle,
+  Webdevheropara,
+  Webdevherotextorientation,
+  Webdevherobackgroundimage,
+  solutionscards,
+  Mainheadingtitle,
+  Mainheadingdescription,
+  Mainheadingtagtext,
+  Mainheadingbackgroundimages,
+  methodscarddata,
+  Calltoactiondata,
+}) {
   return (
     <>
       <MainPageHeroSection
-        title={Webdevdata.webdevelopment.title}
-        paragraph={Webdevdata.webdevelopment.paragraph}
-        textOrientation={Webdevdata.webdevelopment.textOrientation}
-        backgroundImages={Webdevdata.webdevelopment.backgroundImages}
+        title={Webdevherotitle}
+        paragraph={Webdevheropara}
+        textOrientation={Webdevherotextorientation}
+        backgroundImages={Webdevherobackgroundimage}
       />
-      <TypeOneCardSection cards={cards} />
+      <TypeOneCardSection cards={solutionscards} />
       <MainHeadingSection
-        title={Mainheadingsectiondata.webdev.title}
-        description={Mainheadingsectiondata.webdev.description}
-        tagtext={Mainheadingsectiondata.webdev.tagtext}
-        backgroundImages={Mainheadingsectiondata.webdev.backgroundImages}
+        title={Mainheadingtitle}
+        description={Mainheadingdescription}
+        tagtext={Mainheadingtagtext}
+        backgroundImages={Mainheadingbackgroundimages}
       />
-      <FeaturesMethodTypeOne cards={cardssecond} />
+      <FeaturesMethodTypeOne cards={methodscarddata} />
       <CallToAction Calltoactiondata={Calltoactiondata} />
-      {/* <TypeOneCardSection cards={cards} /> */}
     </>
   );
 }
--- src/app/services/webdevelopment/page.js
+++ src/app/services/webdevelopment/page.js
@@ -1,9 +1,47 @@
 import FeaturesTypeOne from "@/app/components/pagecomponents/featuresdetailpagecomponents/featurestypeone";
-
+import Webdevdata from "@/app/data/shareddata/hersectiondata.json";
+import Webdevsolutionscarddata from "@/app/data/shareddata/cardcollectiondata.json";
+import Exampledata from "@/app/data/homedata/homecardcollectiondata.json";
+import Mainheadingsectiondata from "@/app/data/shareddata/mainheadingsectiondata.json";
+import Calltoactiondata from "@/app/data/shareddata/calltoactiondata.json";
 export default function WebDevelopment() {
+  // hero data---------------------------------------------------------------
+  let Webdevherotitle = Webdevdata.webdevelopment.title;
+  let Webdevheropara = Webdevdata.webdevelopment.paragraph;
+  let Webdevherotextorientation = Webdevdata.webdevelopment.textOrientation;
+  let Webdevherobackgroundimage = Webdevdata.webdevelopment.backgroundImages;
+  //--------------------------------------------------------------------------
+
+  // type one card section data-----------------------------------------------
+  let solutionscards = Webdevsolutionscarddata.webdevelopment.cards;
+  //--------------------------------------------------------------------------
+
+  // main heading data--------------------------------------------------------
+  let Mainheadingtitle = Mainheadingsectiondata.webdev.title;
+  let Mainheadingdescription = Mainheadingsectiondata.webdev.description;
+  let Mainheadingtagtext = Mainheadingsectiondata.webdev.tagtext;
+  let Mainheadingbackgroundimages =
+    Mainheadingsectiondata.webdev.backgroundImages;
+
+  // method cards data--------------------------------------------------------
+  let methodscarddata = Exampledata.features.cards;
+  //--------------------------------------------------------------------------
+
   return (
     <>
-      <FeaturesTypeOne />
+      <FeaturesTypeOne
+        Webdevherotitle={Webdevherotitle}
+        Webdevheropara={Webdevheropara}
+        Webdevherotextorientation={Webdevherotextorientation}
+        Webdevherobackgroundimage={Webdevherobackgroundimage}
+        solutionscards={solutionscards}
+        Mainheadingtitle={Mainheadingtitle}
+        Mainheadingdescription={Mainheadingdescription}
+        Mainheadingtagtext={Mainheadingtagtext}
+        Mainheadingbackgroundimages={Mainheadingbackgroundimages}
+        methodscarddata={methodscarddata}
+        Calltoactiondata={Calltoactiondata}
+      />
     </>
   );
 }
