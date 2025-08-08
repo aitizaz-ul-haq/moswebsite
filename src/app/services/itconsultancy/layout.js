deec83d9a4b4ae4354eaa7b770d056b3972cac1b
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-08-08 16:04:30 +0500

    added changes

--- src/app/services/itconsultancy/layout.js
+++ src/app/services/itconsultancy/layout.js
@@ -1,28 +1,6 @@
 // app/club/layout.js
 
-export const metadata = {
-  title: "IT Consultancy | Manage Outsource Services",
-  description: "",
-  //   openGraph: {
-  //     type: "website",
-  //     url: "https://www.biellavita.com/club",
-  //     title: "The Club | BiellaVita",
-  //     description:
-  //       "Experience the BiellaVita lifestyle through exclusive membership.",
-  //     images: [
-  //       {
-  //         url: "https://i.imgur.com/5sQQ2bY.png",
-  //         width: 1200,
-  //         height: 630,
-  //         alt: "Biella Vita - A Private Social Club",
-  //       },
-  //     ],
-  //     site_name: "BiellaVita",
-  //     locale: "en_US",
-  //     "article:section": "Lifestyle",
-  //     "article:tag": ["Social Club", "Well-being", "Lifestyle"],
-  //   },
-};
+
 
 export default function DataManagementLayout({ children }) {
   return <>{children}</>;
