2e84bc774b0fb058e6b80f625b5aedcbc4f01337
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-08-11 17:48:21 +0500

    added changes

--- src/app/components/pagecomponents/careerspagecomponents/novacancysubcomps/novacancyimagecontainer.js
+++ src/app/components/pagecomponents/careerspagecomponents/novacancysubcomps/novacancyimagecontainer.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NoVacancyImageContainer({ logo }) {
   return (
     <div className="no-vacancy-image-container">
-      <Image src={logo} width={120} height={120} />
+      <Image src={logo} width={120} height={120} alt="no-vacancy icon"/>
     </div>
   );
 }
