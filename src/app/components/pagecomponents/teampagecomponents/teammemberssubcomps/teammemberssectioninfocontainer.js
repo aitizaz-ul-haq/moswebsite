faa008793eb63b1062417dedb5a98f6f58736205
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-08-11 17:43:13 +0500

    added changes

--- src/app/components/pagecomponents/teampagecomponents/teammemberssubcomps/teammemberssectioninfocontainer.js
+++ src/app/components/pagecomponents/teampagecomponents/teammemberssubcomps/teammemberssectioninfocontainer.js
@@ -1,9 +1,9 @@
 export default function TeamMembersSectionInfoContainer({memeberheading, memberdesignation}) {
     return(
         <div className="teammemebers-section-info-container">
-            <h4 className="teammemebers-section-name font-poppins">
+            <h3 className="teammemebers-section-name font-poppins">
               {memeberheading}
-            </h4>
+            </h3>
             <p className="teammemebers-section-designation font-nunito">
               {memberdesignation}
             </p>
